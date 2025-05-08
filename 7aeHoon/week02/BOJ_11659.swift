0b686423943dbc447905be23f878d1c5e93f9e77
hoon <stitch8971@gachon.ac.kr>
2025-05-08 16:35:39 +0900

    feat: 20250508 [백준 11659번] 구간 합 구하기 4

--- 7aeHoon/week02/BOJ_11659.swift
+++ 7aeHoon/week02/BOJ_11659.swift
@@ -1,24 +1,46 @@
 import Foundation
 
-let input = readLine()!.components(separatedBy: " ").map { Int($0)! }
-let N = input[0]
-let M = input[1]
+let input = readLine()!.split(separator: " ").map { Int($0)! }
+// N개의 수
+let N: Int = input[0]
+// 합을 구해야 하는 횟수 M
+let M: Int = input[1]
+// 주어진 배열
+let arr: [Int] = readLine()!.split(separator: " ").map { Int($0)! }
 
-var arr = readLine()!.components(separatedBy: " ").map { Int($0)! }
-arr.insert(0, at: 0)
+var pSum: [Int] = Array(repeating: 0, count: 100005)
 
-var sumArr = Array(repeating: 0, count: N+1)
+pSum[0] = arr[0]
 
-sumArr[0] = arr[0]
-
-for i in 1...N {
-    sumArr[i] = sumArr[i-1] + arr[i]
+for i in 1..<N {
+    pSum[i] = pSum[i-1] + arr[i]
 }
 
-for _ in 0..<M {
-    let input = readLine()!.components(separatedBy: " ").map { Int($0)! }
-    let start = input[0]
-    let end = input[1]
+for _ in 1...M {
+    // 주어지는 범위
+    let rangeInput: [Int] = readLine()!.split(separator: " ").map { Int($0)! }
+    // i번째 수
+    let i: Int = rangeInput[0] - 1
+    // j번째 수
+    let j: Int = rangeInput[1] - 1
     
-    print(sumArr[end] - sumArr[start-1])
+    if i - 1 < 0 {
+        print(pSum[j])
+    } else {
+        print(pSum[j] - pSum[i-1])
+    }
+    
 }
+
+/// index  0 1 2 3 4
+/// arr    5 4 3 2 1
+/// psum   5 9 12 14 15
+
+
+/*
+ pSum[1] = pSum[0] + arr[0]
+ pSum[2] = pSum[1] + arr[1]
+ 
+ 
+ 
+ */
